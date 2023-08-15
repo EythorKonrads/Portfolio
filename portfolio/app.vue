5eef565bfae76ab763944ee81bed0c5adb9e29aa
Eyþór Konráðsson <kh.krimper@gmail.com>
2023-08-15 20:32:22 +0200

    adding nuxt

---
+++ portfolio/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>hellooooo</h1>
+    <NuxtLink to="/about">About</NuxtLink>
+  </div>
+  <NuxtPage />
+</template>
---
+++ portfolio/router/index.js
@@ -0,0 +1,17 @@
+{
+    "routes": [
+      {
+        "path": "/about",
+        "component": "pages/about.vue"
+      },
+      {
+        "path": "/",
+        "component": "pages/index.vue"
+      },
+      {
+        "path": "/posts/:id",
+        "component": "pages/posts/[id].vue"
+      }
+    ]
+}
+  
